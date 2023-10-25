676cbbcffb35ba52a66e02fc23fc7b427aaf8b46
akTARDIGRADE13 <itmtha@gmail.com>
2023-10-25 12:14:45 +0900

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: FC = () => {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="./vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
