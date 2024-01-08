11ce9313f6b1357f790fbb75cdf5ae2388730eca
akTARDIGRADE13 <itmtha@gmail.com>
2024-01-08 13:14:44 +0900

    Fix process.env

--- src/study-room/routes/Blog/BlogCategory/BlogArticle/BlogArticle.tsx
+++ src/study-room/routes/Blog/BlogCategory/BlogArticle/BlogArticle.tsx
@@ -1,10 +1,14 @@
 import type React from 'react';
 import { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
 import ReactMarkdown from 'react-markdown';
 import { useParams } from 'react-router-dom';
 import rehypeKatex from 'rehype-katex';
+import rehypeRaw from 'rehype-raw';
 import rehypeSlug from 'rehype-slug';
+import {
+  remarkDefinitionList,
+  defListHastHandlers,
+} from 'remark-definition-list';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import remarkToc from 'remark-toc';
@@ -17,9 +21,6 @@ import MdImage from './components/MdImage';
 import { useMdContent } from './hooks/useMdContent';
 import styles from './BlogArticle.module.css';
 import 'katex/dist/katex.min.css';
-
-const title = import.meta.env.VITE_APP_TITLE;
-console.dir(import.meta.env);
 
 const BlogArticle: React.FC = () => {
   const { category, id } = useParams<{ category: string; id: string }>();
@@ -41,58 +42,48 @@ const BlogArticle: React.FC = () => {
     : null;
 
   const markdownContent = useMdContent(mdPath ?? '');
-  const eyecatchImagePath = `${
-    process.env.NODE_ENV === 'production' ? '/study-room' : ''
-  }/articles/eyecatch/${selectedArticle?.eyecatch}`;
-
-  const domain = 'https://aktardigrade13.github.io/study-room';
-  const homeUrl = `/blog/${category}/${id}`;
-  const mainText = `blog/${category}/${id}`;
 
   return (
-    <>
-      <Helmet
-        title={title}
-        meta={[
-          { name: 'description', content: mainText },
-          { property: 'og:title', content: title },
-          { property: 'og:type', content: 'website' },
-          { property: 'og:url', content: `${domain}${homeUrl}/` },
-          { property: 'og:image', content: `${eyecatchImagePath}` },
-          { property: 'og:description', content: mainText },
-          { name: 'twitter:card', content: 'summary_large_image' },
-          { name: 'twitter:image', content: `${eyecatchImagePath}` },
-        ]}
-      />
-      <Layout>
-        <Container>
-          <Hero title="Blog" subtitle={category ?? 'Default Subtitle'} />
-          {selectedArticle && (
-            <div className={styles['article-wrapper']}>
-              <h1 className={styles['article-title']}>
-                {selectedArticle.title}
-              </h1>
+    <Layout>
+      <Container>
+        <Hero title="Blog" subtitle={category ?? 'Default Subtitle'} />
+        {selectedArticle && (
+          <div className={styles['article-wrapper']}>
+            <h1 className={styles['article-title']}>{selectedArticle.title}</h1>
 
-              <ReactMarkdown
-                remarkPlugins={[
-                  remarkMath,
-                  remarkGfm,
-                  [remarkToc, { maxDepth: 2, heading: '目次' }],
-                ]}
-                rehypePlugins={[rehypeKatex, rehypeSlug]}
-                components={{
-                  pre: (props) => <CodeBlock {...props} />,
-                  img: (props) => <MdImage {...props} />,
-                }}
-                className={styles.content}
-              >
-                {markdownContent}
-              </ReactMarkdown>
+            <div className={styles['article-meta']}>
+              <p>投稿日: {selectedArticle.publishDate}</p>
+              {selectedArticle.lastUpdatedDate !==
+                selectedArticle.publishDate && (
+                <p>最終更新日: {selectedArticle.lastUpdatedDate}</p>
+              )}
             </div>
-          )}
-        </Container>
-      </Layout>
-    </>
+
+            <ReactMarkdown
+              remarkPlugins={[
+                remarkDefinitionList,
+                remarkMath,
+                remarkGfm,
+                [remarkToc, { maxDepth: 3, heading: '目次' }],
+              ]}
+              remarkRehypeOptions={{
+                handlers: {
+                  ...defListHastHandlers,
+                },
+              }}
+              rehypePlugins={[rehypeRaw, rehypeKatex, rehypeSlug]}
+              components={{
+                pre: (props) => <CodeBlock {...props} />,
+                img: (props) => <MdImage {...props} />,
+              }}
+              className={styles.content}
+            >
+              {markdownContent}
+            </ReactMarkdown>
+          </div>
+        )}
+      </Container>
+    </Layout>
   );
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const cherryPickedKeys = [
+  'SOME_KEY_IN_YOUR_ENV_FILE',
+  'SOME_OTHER_KEY_IN_YOUR_ENV_FILE',
+];
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: '/study-room/',
-  resolve: {
-    alias: {
-      '/src/main.tsx': '/src/study-room/main.tsx',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const processEnv = {};
+  cherryPickedKeys.forEach((key) => (processEnv[key] = env[key]));
+
+  return {
+    define: {
+      'process.env': processEnv,
     },
-  },
-  server: {
-    watch: {
-      usePolling: true,
+    plugins: [react(), tsconfigPaths()],
+    base: '/study-room/',
+    resolve: {
+      alias: {
+        '/src/main.tsx': '/src/study-room/main.tsx',
+      },
     },
-  },
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  };
 });
